ea7a56cf857c52f51599fb0ac85808d8d5540a06
vadim <gabderahmanov99@gmail.com>
2017-06-03 12:13:48 +0400

    change formul b

--- src/numbrsFunction/Functions.java
+++ src/numbrsFunction/Functions.java
@@ -53,14 +53,14 @@ public class Functions {
 		if (k == 0 && b < 0) {
 			
 			g.setColor(Color.RED);
-			g.drawLine(0, (h / 2) + b*5 / -2, w, (h / 2) -  b*5 / 2);	
+			g.drawLine(0, h / 2 +  (b*-2), w, h / 2 +  (b*-2));	
 
 		}
 	
 		if (k == 0 && b > 0) {
 			
 			g.setColor(Color.RED);
-			g.drawLine(0, h / 2 -  b*h/w*3, w, h / 2 +  b*-h/w*3);
+			g.drawLine(0, h / 2 -  (b*2), w, h / 2 -  (b*2));
 		}
 		
 		if (k > 0 && b > 0) {
